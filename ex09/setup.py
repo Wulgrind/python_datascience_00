18cfd482b3d6169cb954d701f91dac14981cb4c0
Quentin Brillais <qbrillai@quentins-mbp.home>
2024-11-23 20:10:31 +0100

    added last exercise

---
+++ ex09/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="ft_package", 
+    version="0.0.1",
+    author="qbrillai",
+    author_email="quentbr@gmail.com",
+    description="Package pour remplir l exercice 09 du day 00 de la piscine for data science de 42",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.6"
+)
